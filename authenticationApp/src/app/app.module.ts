6951b7ab780862a306347a38ddae8e4ac8dc3927
JaySeteni <jay.seteni@gmail.com>
2024-05-21 11:39:43 +0200

    2nd commit

--- authenticationApp/src/app/app.module.ts
+++ authenticationApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './comps/admin/admin.component';
+import { HomeComponent } from './comps/home/home.component';
+import { LoginComponent } from './comps/login/login.component';
+import { RegisterComponent } from './comps/register/register.component';
+import { NavbarComponent } from './comps/navbar/navbar.component';
+import { HelpersComponent } from './helpers/helpers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    HelpersComponent
   ],
   imports: [
     BrowserModule,
