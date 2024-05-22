ffea8a4771b98e4f8f0a809a6b4a500afdcaca6c
JaySeteni <jay.seteni@gmail.com>
2024-05-22 07:57:01 +0200

    morning push

--- authenticationApp/src/app/app-routing.module.ts
+++ authenticationApp/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, RouterLink } from '@angular/router';
 import { HomeComponent } from './comps/home/home.component';
 import { RegisterComponent } from './comps/register/register.component';
 import { LoginComponent } from './comps/login/login.component';
 import { NavbarComponent } from './comps/navbar/navbar.component';
-import { RouterLink } from '@angular/router';
 import { AdminComponent } from './comps/admin/admin.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: NavbarComponent },
   {path: 'home', component: HomeComponent},
-  {path: 'signup', component: RegisterComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'admin', component: AdminComponent},
   
@@ -17,7 +17,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
 })
 export class AppRoutingModule { }
 
